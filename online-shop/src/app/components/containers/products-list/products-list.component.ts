eb05e80e68d5f603a156316793f89f7cdb69704a
grozaa <Anamaria.Groza@msg.group>
2023-08-04 17:01:53 +0300

    added some styling

--- online-shop/src/app/components/containers/products-list/products-list.component.ts
+++ online-shop/src/app/components/containers/products-list/products-list.component.ts
@@ -9,8 +9,6 @@ import { IProduct } from 'src/app/types/products.types';
   styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent implements OnInit{
-  // products: Observable<IProduct[]> | undefined =
-  //   this.productsService.getProducts();
   products$: Observable<IProduct[]> | undefined;
 
   constructor(private productsService: ProductsService) {}
